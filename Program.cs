d96f71686a5e68695b0a606572fa5109a1ff8efa
tkylesh <tks2n@mtmail.mtsu.edu>
2022-08-22 11:39:17 -0500

    accidently scaffolded project with .net 6 so I had to change the target framework back to .net6 in order to build and run the project.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using system;
-
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Try to guess the secret number:");
 string? userGuess = Console.ReadLine();
 Console.WriteLine($"You guessed: {userGuess}");
