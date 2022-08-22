b64a88bf228cf5bef644195483ca7b35aed6de6a
tkylesh <tks2n@mtmail.mtsu.edu>
2022-08-22 11:35:19 -0500

    removing vscode debug assets from repository

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using system;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Try to guess the secret number:");
+string? userGuess = Console.ReadLine();
+Console.WriteLine($"You guessed: {userGuess}");
