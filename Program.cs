8ed535010a1beffccd9eee9023dc272a5eead713
tkylesh <tks2n@mtmail.mtsu.edu>
2022-08-22 12:55:23 -0500

    phase2 complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//Phase 1
 Console.WriteLine("Try to guess the secret number:");
-string? userGuess = Console.ReadLine();
-Console.WriteLine($"You guessed: {userGuess}");
+string? userInput = Console.ReadLine();
+// Console.WriteLine($"You guessed: {userGuess}");
+
+//Phase 2
+//create a variable to contain secret number
+int secretNumber = 42;
+/*compare users guess with secret number. display
+  a success message if correct, failure message otherwise*/
+int userGuess;
+bool guessValid = int.TryParse(userInput, out userGuess);
+
+if (guessValid)
+{
+    if (userGuess == secretNumber)
+    {
+        Console.WriteLine($"Your guess is Correct!");
+    }
+    else
+    {
+        Console.WriteLine($"Your guess is wrong! try again but this time with feeling!");
+    }
+}
